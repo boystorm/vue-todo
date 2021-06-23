fc0de9b2521deffdd0608a081fef4929c96405b2
boystorm <motokrzr11@gmail.com>
2021-06-23 22:25:18 +0900

    리스트 출력

--- src/components/todolist.vue
+++ src/components/todolist.vue
@@ -1,11 +1,29 @@
 <template>
     <ul>
-        <li></li>
+        <li v-for="todoList in todoLists" v-bind:key="todoList">{{ todoList }}</li>
     </ul>
 </template>
 
 <script>
+// ul li에 data 스토리지에 있는 데이터 넣어주기
 
+export default { 
+    data (){
+        return {
+            todoLists: []
+        }
+    },
+    created() {
+        if(localStorage.length > 0){
+            for(let i = 0; i < localStorage.length ; i++) {
+                if((localStorage.key(i) != 'loglevel:webpack-dev-server') && (localStorage.key(i) != '')){
+                    this.todoLists.push(localStorage.key(i));
+                }
+            }
+        }
+    }
+    
+}
 </script>
 
 
